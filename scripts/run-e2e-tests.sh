45362ab2545394f7d020b0dffee667079f7584a4
Annamaria Szegedi <aszegedi@hortonworks.com>
2017-10-26 18:54:04 +0200

    introduce unsafe-perm to the scripts

--- scripts/run-e2e-tests.sh
+++ scripts/run-e2e-tests.sh
@@ -14,7 +14,7 @@ echo "Protractor version: " $(protractor --version)
 echo "WebDriver version: " $(webdriver-manager version)
 
 # Install the necessary packages
-npm install
+npm install --unsafe-perm
 # Run the Selenium installation script, located in the local node_modules/ directory.
 # This script downloads the files required to run Selenium itself and build a start script and a directory with them.
 # When this script is finished, we can start the standalone version of Selenium with the Chrome driver by executing the start script.
